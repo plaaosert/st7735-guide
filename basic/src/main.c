5572a137a382a9e346006181e125182a6bc0a9cc
plaaosert <terrariandallas@gmail.com>
2022-03-17 18:26:49 +0000

    Removed plaao

--- basic/src/main.c
+++ basic/src/main.c
@@ -10,6 +10,6 @@ int main() {
   setvbuf ( stdout , NULL , _IONBF , 0 );  // Disable line and block buffering on stdout (for talking through serial)
   sleep_ms(1000);  // Give the Pico some time to think...
   ST7735_Init();  // Initialise the screen
-  ST7735_DrawImage(0, 0, 80, 160, plaao_logo);  // Draw the image.
+  ST7735_DrawImage(0, 0, 80, 160, arducam_logo);  // Draw the image.
   sleep_ms(20000);  // Sleep for 20 seconds
 }
